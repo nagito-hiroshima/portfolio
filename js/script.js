c9cae6f4a9b0d6969b50f264a388e999ef5723de
なぎと-nagito- <hello@nagito.work>
2023-05-28 01:49:16 +0900

    updeta

--- js/script.js
+++ js/script.js
@@ -18,3 +18,48 @@ $(function () {
   });
 
 });
+
+
+  // スライドショーの画像リスト
+  var slideImages = [
+    "./img/back/main.png",
+    "./img/back/private.png",
+    "./img/back/relax.png",
+    // 他の画像ファイルのパスを追加する
+  ];
+    // スライドショーの現在のインデックス
+    var currentIndex = 0;
+  
+    // スライドショーの要素を取得
+    var slideshow = document.querySelector(".main-visual");
+  
+    // 初回のスライド表示を開始する
+    changeSlide();
+  
+    // 画像を切り替える関数
+    function changeSlide() {
+      // インデックスが画像の数を超えた場合は最初の画像に戻る
+      if (currentIndex >= slideImages.length) {
+        currentIndex = 0;
+      }
+  
+  
+      // 0.5秒後にフェードアウトのアニメーションを終了し、背景画像を切り替える
+      setTimeout(function() {
+  
+        // 背景画像を切り替える
+        slideshow.style.backgroundImage = "url(" + slideImages[currentIndex] + ")";
+  
+        // 0.5秒後にフェードインのアニメーションを終了し、次のスライドを表示する
+        setTimeout(function() {
+  
+          // インデックスをインクリメント
+          currentIndex++;
+  
+          // 次のスライドを一定の間隔で表示する（5000ミリ秒＝5秒）
+          setTimeout(changeSlide, 5000);
+        }, 500);
+      }, 500);
+    }
+  // 初回のスライド表示を開始する
+  changeSlide();
